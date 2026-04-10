6bd84fa10defca94f3e9899f3c34b8d3ef2f8c75
TradeCraftEducation <joseph.molina@tradecrafteducation.com>
2026-04-10 14:08:59 -0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -107,6 +107,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
 
         mode: 'payment',
+        customer_email: buyer_email, // <--- ADD THIS LINE
 
         line_items: [
 
